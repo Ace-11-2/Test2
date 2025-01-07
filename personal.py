82fd78dc039214b707656431245a6a15968ae389
Ace-11-2 <asinthdeva@gmail.com>
2025-01-07 18:49:03 +0530

    first commit

---
+++ personal.py
@@ -0,0 +1,18 @@
+result = {
+    "name": "Asinth",
+    "age": 13,
+    "grade": 8,
+    "school": "Study",
+    "city": "Puducherry",
+    "state": "Puducherry",
+    "country": "India",
+    "continent": "Asia",
+    "planet": "Earth",
+    "galaxy": "Milky Way",
+    "universe": "Universe",
+    "hobbies": ["Coding", "Reading", "Gaming", "Watching Movies"],
+    "languages": ["Tamil", "English"],
+    "programming_languages":"Python",
+    "tools": ["VSCode", "IDLE"],
+    
+    }
