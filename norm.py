7990dc56f5b0f74b833900d8cd4a74c470b10bb5
Ace-11-2 <asinthdeva@gmail.com>
2025-01-07 18:56:06 +0530

    2nd commit

---
+++ norm.py
@@ -0,0 +1,6 @@
+start = int(input("Enter the start value: "))
+end = int(input("Enter the end value: "))
+step = int(input("Enter the step value: "))
+
+for i in range(start, end, step):
+    print(i)
